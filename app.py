f4e78de05b89a5af0242dfaf5bfe7854e032742f
hzertz0 <darwenzhu1221@gmail.com>
2025-03-14 10:14:21 -0400

    :(

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 import json
 
-role = input("Are you a teacher or student? ")
-if role.lower() == "teacher":
+class Flashcard:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+questions = [
+    Flashcard("What are the sides you use to calculate sine?", "Opposite/Hypotenuse"),
+    Flashcard("What are the sides you use to calculate cosine?", "Adjacent/Hypotenuse"),
+    Flashcard("What are the sides you use to calculate tangent?", "Opposite/Adjacent")
+]
+
+try:
+    with open("questions.json", "r") as file:
+        questions_data = json.load(file)
+
+except FileNotFoundError:
+    questions_data = []
+
+questions_data = [Flashcard.to.dict() for flashcard in questions]
+
+with open("questions.json", "w") as file:
+    json.dump(questions_data, file, indent=2)
+
+role = input("Are you a teacher or student? ").lower
+if role() == "teacher":
     print("test")
-if role.lower() == "student":
+if role() == "student":
     print("test2")
+
