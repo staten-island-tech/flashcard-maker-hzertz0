c16f5a4b6204052ebe4926beb9b303d21efb3ecc
hzertz0 <darwenzhu1221@gmail.com>
2025-03-20 10:14:49 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ class Flashcard:
     def __init__(self, question, answer):
         self.question = question
         self.answer = answer
+    
+    def display_info(self):
+        return f"{self.year} {self.make} {self.model}"
+    
+    def to_dict(self):
+        return {
+            "question": self.question,
+            "answer": self.answer
+        }
 
 questions = [
     Flashcard("What are the sides you use to calculate sine?", "Opposite/Hypotenuse"),
@@ -18,14 +27,14 @@ try:
 except FileNotFoundError:
     questions_data = []
 
-questions_data = [Flashcard.to_dict() for flashcard in questions]
+questions_data = [Flashcard.to_dict() for Flashcard in questions]
 
 with open("questions.json", "w") as file:
     json.dump(questions_data, file, indent=2)
 
 role = input("Are you a teacher or student? ").lower
 if role() == "teacher":
-    print("test")
+    asked = input("Input phrase & answer: ")
 if role() == "student":
-    print("test2")
+    print()
 
